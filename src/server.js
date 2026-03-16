be02a43d2e5fa97c27991a65aaa9f25cb3911a51
Kostya <kostyagolovchik@gmail.com>
2026-03-16 18:36:43 +0100

    fix

--- src/server.js
+++ src/server.js
@@ -22,5 +22,5 @@ await connectMongoDB();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('server is running');
+  console.log(`server is running on port ${PORT}`);
 });
