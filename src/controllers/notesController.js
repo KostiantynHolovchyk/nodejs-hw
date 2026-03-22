0949aabde8e433e16b7802bbcb443fab9ef3c47f
Kostya <kostyagolovchik@gmail.com>
2026-03-22 15:04:06 +0100

    Update notesController.js

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -5,18 +5,19 @@ export const getAllNotes = async (req, res) => {
   const { tag, search, page = 1, perPage = 10 } = req.query;
   const pageNumber = Number(page);
   const perPageNumber = Number(perPage);
-  const filter = {};
+  const skip = (pageNumber - 1) * perPageNumber;
+  let query = Note.find();
   if (tag) {
-    filter.tag = tag;
+    query = query.where('tag').equals(tag);
   }
   if (search) {
-    filter.$text = { $search: search };
+    query = query.find({ $text: { $search: search } });
   }
-  const skip = (pageNumber - 1) * perPageNumber;
-  const totalNotes = await Note.countDocuments(filter);
-  const notes = await Note.find(filter).skip(skip).limit(perPageNumber);
+  const [notes, totalNotes] = await Promise.all([
+    query.clone().skip(skip).limit(perPageNumber),
+    query.clone().countDocuments(),
+  ]);
   const totalPages = Math.ceil(totalNotes / perPageNumber);
-
   res.status(200).json({
     page: pageNumber,
     perPage: perPageNumber,
@@ -25,45 +26,37 @@ export const getAllNotes = async (req, res) => {
     notes,
   });
 };
+
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
-
   const note = await Note.findById(noteId);
-
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
-
   res.status(200).json(note);
 };
 
 export const createNote = async (req, res) => {
   const note = await Note.create(req.body);
-
   res.status(201).json(note);
 };
 
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
-
   const note = await Note.findByIdAndDelete(noteId);
-
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
-
   res.status(200).json(note);
 };
+
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
-
   const note = await Note.findByIdAndUpdate(noteId, req.body, {
     returnDocument: 'after',
   });
-
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
-
   res.status(200).json(note);
 };
