a2928d50557fda4570b548ceed2cd8da0a810f5e
Kostya <kostyagolovchik@gmail.com>
2026-03-24 09:07:13 +0100

    user.js

--- src/models/user.js
+++ src/models/user.js
@@ -3,16 +3,18 @@ import { model, Schema } from 'mongoose';
 const userSchema = new Schema(
   {
     username: { type: String, trim: true },
-    email: { type: String, required: true, trim: true },
+    email: { type: String, required: true, trim: true, unique: true },
     password: { type: String, required: true, minlength: 8 },
   },
   { timestamps: true },
 );
+
 userSchema.pre('save', function () {
   if (!this.username) {
     this.username = this.email;
   }
 });
+
 userSchema.methods.toJSON = function () {
   const obj = this.toObject();
   delete obj.password;
